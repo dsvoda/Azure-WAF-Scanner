05f5308f794eaa5c7654a3434b3a7a3858a0e726
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2025-10-21 14:53:32 -0500

    Update WafCheck ID format in Invoke.ps1

--- modules/Pillars/Security/SE11/Invoke.ps1
+++ modules/Pillars/Security/SE11/Invoke.ps1
@@ -1,10 +1,10 @@
 
-Register-WafCheck -Pillar 'Security' -Id 'SE:11' -Name 'Security testing regimen' -Description 'Vulnerability assessment signals' -InvokeScript {
+Register-WafCheck -Pillar 'Security' -Id 'SE11' -Name 'Security testing regimen' -Description 'Vulnerability assessment signals' -InvokeScript {
   param([string]$SubscriptionId)
 
   $assess = Get-DefenderAssessments -SubscriptionId $SubscriptionId | Where-Object { $_.DisplayName -match 'vulnerab' }
   $status = ($assess.Count -gt 0) ? 'Warn' : 'Manual'
-  New-WafResult -Pillar 'Security' -Id 'SE:11' -Name 'Security testing regimen' -Description 'Vulnerability assessments present' `
+  New-WafResult -Pillar 'Security' -Id 'SE11' -Name 'Security testing regimen' -Description 'Vulnerability assessments present' `
     -SubscriptionId $SubscriptionId -TestMethod 'Defender Assessments' -Status $status -Score (Convert-StatusToScore $status) `
     -Evidence ("VA_Findings={0}" -f $assess.Count) -Recommendation 'Enable VM/Container/DB vulnerability assessment in Defender; test detection rules'
 
