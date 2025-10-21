3a9337921a71ec0fb4675ae58347b433a27dd1b2
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2025-10-21 14:45:25 -0500

    Fix ID format in WafCheck and WafResult calls

--- modules/Pillars/PerformanceEfficiency/PE07/Invoke.ps1
+++ modules/Pillars/PerformanceEfficiency/PE07/Invoke.ps1
@@ -1,10 +1,10 @@
 
-Register-WafCheck -Pillar 'Performance Efficiency' -Id 'PE:07' -Name 'Optimize code & infrastructure' -Description 'Advisor performance items' -InvokeScript {
+Register-WafCheck -Pillar 'Performance Efficiency' -Id 'PE07' -Name 'Optimize code & infrastructure' -Description 'Advisor performance items' -InvokeScript {
   param([string]$SubscriptionId)
 
   $adv = Get-Advisor -SubscriptionId $SubscriptionId -Category @('Performance')
   $status = ($adv.Count -gt 0) ? 'Warn' : 'Pass'
-  New-WafResult -Pillar 'Performance Efficiency' -Id 'PE:07' -Name 'Optimize code & infrastructure' -Description 'Advisor performance items' `
+  New-WafResult -Pillar 'Performance Efficiency' -Id 'PE07' -Name 'Optimize code & infrastructure' -Description 'Advisor performance items' `
     -SubscriptionId $SubscriptionId -TestMethod 'Advisor' -Status $status -Score (Convert-StatusToScore $status) `
     -Evidence ("AdvisorPerfRecs={0}" -f $adv.Count) -Recommendation 'Use platform features; remove unnecessary work from app code'
 
