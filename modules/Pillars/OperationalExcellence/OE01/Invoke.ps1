9a7d9b61b94b121b3e65d1454e0d26807bfa36fb
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2025-10-21 14:39:05 -0500

    Fix ID format in WafCheck and WafResult commands

--- modules/Pillars/OperationalExcellence/OE01/Invoke.ps1
+++ modules/Pillars/OperationalExcellence/OE01/Invoke.ps1
@@ -1,9 +1,9 @@
 
-Register-WafCheck -Pillar 'Operational Excellence' -Id 'OE:01' -Name 'Standard practices defined' -Description 'Manual: standards/process culture' -InvokeScript {
+Register-WafCheck -Pillar 'Operational Excellence' -Id 'OE01' -Name 'Standard practices defined' -Description 'Manual: standards/process culture' -InvokeScript {
   param([string]$SubscriptionId)
 
   $status = 'Manual'
-  New-WafResult -Pillar 'Operational Excellence' -Id 'OE:01' -Name 'Standard practices defined' -Description 'Document standards; blameless culture; CI' `
+  New-WafResult -Pillar 'Operational Excellence' -Id 'OE01' -Name 'Standard practices defined' -Description 'Document standards; blameless culture; CI' `
     -SubscriptionId $SubscriptionId -TestMethod 'Manual' -Status $status -Score (Convert-StatusToScore $status) `
     -Evidence 'Standards not discoverable from Azure APIs' -Recommendation 'Publish standards; measure code reviews, deployment frequency, MTTR'
 
