ad37fff6838d8416c41c5f6e127e55fa6d03ff97
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2025-10-21 14:38:36 -0500

    Update WafCheck ID format in Invoke.ps1

--- modules/Pillars/CostOptimization/CO14/Invoke.ps1
+++ modules/Pillars/CostOptimization/CO14/Invoke.ps1
@@ -1,10 +1,10 @@
 
-Register-WafCheck -Pillar 'Cost Optimization' -Id 'CO:14' -Name 'Consolidate resources & responsibility' -Description 'Identify single-resource RGs (approx)' -InvokeScript {
+Register-WafCheck -Pillar 'Cost Optimization' -Id 'CO14' -Name 'Consolidate resources & responsibility' -Description 'Identify single-resource RGs (approx)' -InvokeScript {
   param([string]$SubscriptionId)
 
   $one = Invoke-Arg -Kql "resources | summarize c=count() by resourceGroup | where c==1 | summarize rg=count()" -Subscriptions $SubscriptionId
   $status = ($one[0].rg -gt 0) ? 'Warn' : 'Pass'
-  New-WafResult -Pillar 'Cost Optimization' -Id 'CO:14' -Name 'Consolidate resources & responsibility' -Description 'RGs with a single resource' `
+  New-WafResult -Pillar 'Cost Optimization' -Id 'CO14' -Name 'Consolidate resources & responsibility' -Description 'RGs with a single resource' `
     -SubscriptionId $SubscriptionId -TestMethod 'ARG' -Status $status -Score (Convert-StatusToScore $status) `
     -Evidence ("SingleResourceRGs={0}" -f $one[0].rg) -Recommendation 'Consolidate where appropriate; centralize shared services to increase density'
 
