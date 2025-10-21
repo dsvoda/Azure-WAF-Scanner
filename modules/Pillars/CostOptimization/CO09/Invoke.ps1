04015b3d108b939f3772e99d9dc0b20b717081b9
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2025-10-21 14:36:53 -0500

    Update WafCheck ID format in Invoke.ps1

--- modules/Pillars/CostOptimization/CO09/Invoke.ps1
+++ modules/Pillars/CostOptimization/CO09/Invoke.ps1
@@ -1,10 +1,10 @@
 
-Register-WafCheck -Pillar 'Cost Optimization' -Id 'CO:09' -Name 'Optimize flow costs' -Description 'Tags for flows/priorities (approx)' -InvokeScript {
+Register-WafCheck -Pillar 'Cost Optimization' -Id 'CO09' -Name 'Optimize flow costs' -Description 'Tags for flows/priorities (approx)' -InvokeScript {
   param([string]$SubscriptionId)
 
   $flow = Invoke-Arg -Kql "resources | where isnotempty(tags.['flow']) or isnotempty(tags.['criticality']) | summarize c=count()" -Subscriptions $SubscriptionId
   $status = ($flow[0].c -gt 0) ? 'Warn' : 'Manual'
-  New-WafResult -Pillar 'Cost Optimization' -Id 'CO:09' -Name 'Optimize flow costs' -Description 'Flow/criticality tags present' `
+  New-WafResult -Pillar 'Cost Optimization' -Id 'CO09' -Name 'Optimize flow costs' -Description 'Flow/criticality tags present' `
     -SubscriptionId $SubscriptionId -TestMethod 'ARG tags' -Status $status -Score (Convert-StatusToScore $status) `
     -Evidence ("TaggedResources={0}" -f $flow[0].c) -Recommendation 'Tag flows & priorities; align spend to flow importance'
 
